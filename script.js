380769772837782e41621095b88bd59aaaacd87f
sam hockaday <samuel.hockaday@gmail.com>
2023-10-23 19:19:17 +0100

    Coded JS for mobile menu using function.

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const mobileNav = () => {
-const headerBtn = document.querySelector('header__bars');
-const mobileNav = document.querySelector('.mobile-nav');
+    const headerBtn = document.querySelector('.header__bars'); // Corrected selector
+    const mobileNav = document.querySelector('.mobile-nav');
 
- headerBtn.addEventListener('click', () => {mobileNav.getElementsByClassName.display = 'flex';
-});
-};
+  //status of button 
+    let isMobileNavOpen = false;
+
+
+    headerBtn.addEventListener('click', () => {
+        isMobileNavOpen = !isMobileNavOpen;
+        if (isMobileNavOpen) {
+            mobileNav.style.display = 'flex';
+            document.body.style.overflowY = 'hidden';
+
+        } else {
+      mobileNav.style.display = 'none'; // Corrected property assignment
+      document.body.style.overflowY = 'auto';
+}});
+  };
+   
+  mobileNav();
+  
