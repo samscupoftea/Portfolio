9da67873f2716b3b1097c0f89722ed3bd25a14ab
sam hockaday <samuel.hockaday@gmail.com>
2023-10-23 19:51:55 +0100

    Fixed my forEach Loop which wasn't working.

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 const mobileNav = () => {
     const headerBtn = document.querySelector('.header__bars'); // Corrected selector
     const mobileNav = document.querySelector('.mobile-nav');
-    const mobileLinks = document.querySelectorAll('mobile-nav__link');
-  //status of button 
-    let isMobileNavOpen = false;
+    const mobileLinks = document.querySelectorAll('.mobile-nav__link'); // Corrected selector
 
+    // Status of the button 
+    let isMobileNavOpen = false;
 
     headerBtn.addEventListener('click', () => {
         isMobileNavOpen = !isMobileNavOpen;
         if (isMobileNavOpen) {
             mobileNav.style.display = 'flex';
             document.body.style.overflowY = 'hidden';
-
         } else {
-      mobileNav.style.display = 'none'; // Corrected property assignment
-      document.body.style.overflowY = 'auto';
-}
-});
-mobileLinks.forEach(Link => {
-    link.addEventListener('click', () => {
-        isMobileNavOpen = false;
-        mobileNav.style.display = 'none';
-        document.body.style.overflowY = 'auto';
-
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        }
     });
-});
 
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        });
+    });
+};
 
-  };
-   
-  mobileNav();
-  
+mobileNav();
