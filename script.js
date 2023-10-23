88c199021a75ab791cb77249eabafd88b947d4c5
sam hockaday <samuel.hockaday@gmail.com>
2023-10-23 18:53:13 +0100

    Javascript added function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const mobileNav = () => {
 const headerBtn = document.querySelector('header__bars');
+const mobileNav = document.querySelector('.mobile-nav');
+
+ headerBtn.addEventListener('click', () => {mobileNav.getElementsByClassName.display = 'flex';
+});
+};
